a1dd50a0dde89371f134b10034c75cebc2a5ba7c
rona <ronabucio@gmail.com>
2024-02-06 10:35:38 +0800

    adding app.js

--- app.js
+++ app.js
@@ -21,17 +21,19 @@ async function fetchAndDisplayProducts() {
             const productName = document.createElement('h3');
             productName.textContent = product.name;
             
-            const productDate = document.createElement('h4');
-            productName.textContent = product.date-added;
+            // const productDate = document.createElement('h4');
+            // productDate.textContent = product.date-added;
 
-            const productDescrip = document.createElement('h2');
-            productName.textContent = product.description;
+            // const productDescrip = document.createElement('h2');
+            // productDescrip.textContent = product.description;
 
             const productPrice = document.createElement('p');
             productPrice.textContent = `Price: $${product.price}`;
 
             productCard.appendChild(productName);
             productCard.appendChild(productPrice);
+            // productCard.appendChild(productDescrip);
+            // productCard.appendChild(productDate);
 
             productList.appendChild(productCard);
         });
